6cef87624c7aaaad4b5a3c60b1c7d6023162e89f
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-01-04 14:16:23 -0300

    feat: create first connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello ^-^');
+import { config } from 'dotenv';
+config();
+
+import { Client } from 'discord.js';
+const client: Client = new Client();
+
+client.on('ready', () => {
+  console.log('Bot is ready!');
+});
+
+client.login(process.env.BOT_TOKEN);
